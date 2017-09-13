34913281cc5011bf81bf2f1e76bfde258517b83b
Attilio Priolo <a.priolo@infosolution.it>
2017-09-13 15:57:18 +0200

    Added extern "C" to allow cplusplus compilation

--- include/darknet.h
+++ include/darknet.h
@@ -32,6 +32,10 @@ extern int gpu_index;
     #endif
 #endif
 
+#ifdef __cplusplus
+    extern "C"{
+#endif
+
 typedef struct{
     int classes;
     char **names;
@@ -754,5 +758,9 @@ void normalize_array(float *a, int n);
 int *read_intlist(char *s, int *n, int d);
 size_t rand_size_t();
 float rand_normal();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
