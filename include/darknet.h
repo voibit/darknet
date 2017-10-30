993fa8ed334b561060eaad16d71558a60e5fafa9
voibit <krestean@gmail.com>
2017-10-30 12:10:26 +0100

    added reading float from config

--- include/darknet.h
+++ include/darknet.h
@@ -645,7 +645,7 @@ void get_detection_boxes(layer l, int w, int h, float thresh, float **probs, box
 
 char *option_find_str(list *l, char *key, char *def);
 int option_find_int(list *l, char *key, int def);
-
+float option_find_float(list *l, char *key, float def);
 network *parse_network_cfg(char *filename);
 void save_weights(network *net, char *filename);
 void load_weights(network *net, char *filename);
